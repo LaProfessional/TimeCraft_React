358aa7a17eb16f2fdac38bc1580cb5ed495050ea
LaProfessional <118113982+LaProfessional@users.noreply.github.com>
2025-03-07 01:36:15 +0700

    ㅤ

--- frontend/src/components/navigation/Navigation.jsx
+++ frontend/src/components/navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Button from './Button';
 import QuickSearch from "./QuickSearch";
 import TasksSortOptions from "./TasksSortOptions";
